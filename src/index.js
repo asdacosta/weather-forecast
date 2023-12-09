dc54a55b0e68767f47dd40ce156a709e85cdf874
asdacosta <asdacosta18@gmail.com>
2023-12-09 08:37:03 +0000

    Add getData function to retreive data from API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import './reset.css';
-import './style.css';
-// import './assets/###.mp4';
+import "./reset.css";
+import "./style.css";
 
 const importAllAssets = (function () {
   function importAll(r) {
     return r.keys().map(r);
   }
 
-  const assets = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
+  const assets = importAll(require.context("./assets", false, /\.(png|jpe?g|svg)$/));
 })();
+
+const getData = async function (location) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=06393eb913004a98bfe70936230812&q=${location}`,
+    { mode: cors },
+  );
+  const weatherData = await response.json();
+
+  return weatherData;
+};
